95de909a06bf2408753dfaca751a517a059f8fc3
ode233 <ody122333@gmail.com>
2023-02-23 01:52:03 +0800

    update

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 Build locally
 
 ```
-docker build --tag idena .
-docker run -d -p 9009:9009 -v /root/idena/datadir:/datadir --name idena --restart=always idena
+docker build . -t idena-node
+docker run -d -p 9009:9009 -v /root/idena/datadir:/datadir --name idena-node --restart=always idena-node
 ```
 
 Or use image form [dockerhub](https://hub.docker.com/r/ode233/idena-node)
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-docker container stop idena
-docker container rm idena
-docker build . -t idena
-docker run -d -p 9009:9009 -v /root/idena/datadir:/datadir --name idena --restart=always idena
+docker container stop idena-node
+docker container rm idena-node
+docker build . -t idena-node
+docker run -d -p 9009:9009 -v /root/idena/datadir:/datadir --name idena-node --restart=always idena-node
